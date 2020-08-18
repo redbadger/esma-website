c4a09639240d3f50c02bab2d6c7c3717f88055b2
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-08-18 17:38:41 +0200

    import UkSvg

--- src/components/key-stats/uk-overlay.tsx
+++ src/components/key-stats/uk-overlay.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import { ReactComponent as Uk } from "./uk.svg";
-const ukSvg = require("./uk.svg") as string;
+import UkSvg from "./uk.svg";
 
 const UkOverlay = ({ children }: { children: React.SVGProps<SVGElement> }) => (
   <>
     <svg width="360" height="591" viewBox="0 0 360 591">
       <title>Uk Overlay</title>
-      <image x="0" y="0" width="360" height="591" href={ukSvg} />
+      <UkSvg />
       {children}
     </svg>
   </>
