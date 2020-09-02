8d573ef9247f5b96045b9c93826161446a911c33
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-02 13:16:00 +0100

    correcting Pills test
    updating snapshots for pills

--- src/components/research/pills.test.tsx
+++ src/components/research/pills.test.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Pills from "./pills";
+import { PillData } from "./types";
 
 describe("Pills", () => {
-    const samplePills = [
-        { name: "Executive Summary", href: "/", isActive: true },
-        { name: "Early Years Foundation Stage", href: "/" },
-        { name: "Family Circumstances", href: "/" },
-        { name: "Social class and underachievement", href: "/" },
-        { name: "School cultures", href: "/" },
-        { name: "Geographical educational inequality", href: "/" },
-        { name: "Type and quality of schooling", href: "/" },
-        { name: "Low aspirations", href: "/" },
-        { name: "Access to career advice", href: "/" },
-        { name: "Gaps in attainnment", href: "/" },
-        { name: "Impact of COVID-19", href: "/" }
-    ];
+  const samplePills: PillData[] = [
+    { name: "Executive Summary", href: "/" },
+    { name: "Early Years Foundation Stage", href: "/" },
+    { name: "Family Circumstances", href: "/" },
+    { name: "Social class and underachievement", href: "/" },
+    { name: "School cultures", href: "/" },
+    { name: "Geographical educational inequality", href: "/" },
+    { name: "Type and quality of schooling", href: "/" },
+    { name: "Low aspirations", href: "/" },
+    { name: "Access to career advice", href: "/" },
+    { name: "Gaps in attainment", href: "/" },
+    { name: "Impact of Covid-19", href: "/" },
+  ];
 
   const colorActive = "yellow";
 
   it("renders correctly", () => {
     const tree = renderer
-      .create(<Pills pills={samplePills} colorActive={colorActive} />)
+      .create(
+        <Pills
+          pills={samplePills}
+          colorActive={colorActive}
+          currentPillIndex={0}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
