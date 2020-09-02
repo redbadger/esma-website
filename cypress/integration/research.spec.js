cb457dba30dc8b52eb86069e5fdb0797af881be3
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-02 14:21:25 +0100

    useful Cypress tests checking Pills and PrevNext

--- cypress/integration/research.spec.js
+++ cypress/integration/research.spec.js
@@ -6,4 +6,34 @@ describe("Research page", function () {
     cy.get(".scroll-help.right").click();
     cy.contains("Working life").then(el => !Cypress.dom.isHidden(el));
   });
+
+  it("has Next Up button which takes the user to the next page", function () {
+    cy.server();
+    cy.visit("/research/early-years/executive-summary");
+    cy.get("h2").contains("Life stage overview");
+    cy.contains("Next Up").click();
+    cy.get("h2").contains("Family environment");
+    cy.contains("Next Up").click();
+    cy.get("h2").contains("Housing and the neighbourhood");
+  });
+
+  it("has Previous button which takes the user to the previous page", function () {
+    cy.server();
+    cy.visit("/research/early-years/housing-and-neighbourhood");
+    cy.get("h2").contains("Housing and the neighbourhood");
+    cy.contains("Previous").click();
+    cy.get("h2").contains("Family environment");
+    cy.contains("Previous").click();
+    cy.get("h2").contains("Life stage overview");
+  });
+
+  it("has pills which takes the user to those pages", function () {
+    cy.server();
+    cy.visit("/research/early-years/executive-summary");
+    cy.get("h2").contains("Life stage overview");
+    cy.get("nav").contains("Housing and the neighbourhood").click();
+    cy.get("h2").contains("Housing and the neighbourhood");
+    cy.get("nav").contains("Impact of Covid-19").click();
+    cy.get("h2").contains("Impact of Covid-19");
+  });
 });
