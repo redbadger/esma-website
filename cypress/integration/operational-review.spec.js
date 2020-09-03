539cdfebd7ec153bb438cd71218db787193ba83d
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-03 17:11:01 +0100

    Cypress test for Operational Review page

---
+++ cypress/integration/operational-review.spec.js
@@ -0,0 +1,17 @@
+describe("Operational review page", function () {
+  it("can be found from homepage", function () {
+    cy.server();
+    cy.visit("/");
+    cy.contains("Operational review").click();
+    cy.contains("Coming September 2021");
+  });
+
+  it("lists Andrew Garard on page load, then has Martin Lewis in Third Sector view", function () {
+    cy.server();
+    cy.visit("/operational-review");
+    cy.get("h2").contains("Operational Review");
+    cy.contains("Andrew Garard");
+    cy.contains("Third Sector").click();
+    cy.contains("Martin Lewis");
+  });
+});
