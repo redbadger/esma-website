4bacfd8d6ebda7d7526fd73f29cf4be9854fce99
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-03 15:14:18 +0100

    relief image size and focus in three screen sizes

--- src/components/operational-review/main.tsx
+++ src/components/operational-review/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { css } from "@emotion/core";
 import ReliefImage from "./relief-image";
+import { BreakPoint, mq } from "../../util/mq";
 
 const mainCss = css`
   padding: 0.75rem;
@@ -27,40 +28,67 @@ const mainCss = css`
   p {
     padding: 1rem 0;
   }
+
+  ${mq(BreakPoint.md)} {
+    aside .hero-image-wrapper {
+      height: 15rem;
+
+      picture img {
+        object-position: center -5rem;
+      }
+    }
+  }
+  ${mq(BreakPoint.lg)} {
+    display: flex;
+
+    aside .hero-image-wrapper {
+      width: 27.5rem;
+      height: 37.5rem;
+
+      picture img {
+        object-position: -7rem 0rem;
+      }
+    }
+  }
 `;
 
 const Main = () => (
-  <main css={mainCss}>
-    <p className="intro">
-      Following the construction of the Social Mobility Activity Map the Study
-      Segment teams will:
-    </p>
-    <ul>
-      <li>
-        In respect of each initiative and organisation mapped, make
-        recommendations on how the programmes focused on the Study Segment could
-        better collaborate e.g. formation of “Issue Communities” to avoid
-        duplication of provision/administration (e.g. student on-boarding),
-        merger of initiatives and/or adoption of a ‘baton pass’ measures between
-        adjoining age group focussed schemes;
-      </li>
-      <li>
-        Create a summary of gaps in provision e.g. issues in the SM Chronology
-        of Issues not addressed or lack of activity in specific geographic
-        areas;
-      </li>
-      <li>
-        Highlight best in class providers measured on reach and collaboration;
-        and
-      </li>
-      <li>Highlight additional areas flagged for innovation focus.</li>
-    </ul>
-    <p>
-      The Study Segment teams will then present their findings to the ESMA panel
-      in September 2021
-    </p>
-    <ReliefImage />
-  </main>
+  <div css={mainCss}>
+    <main>
+      <p className="intro">
+        Following the construction of the Social Mobility Activity Map the Study
+        Segment teams will:
+      </p>
+
+      <ul>
+        <li>
+          In respect of each initiative and organisation mapped, make
+          recommendations on how the programmes focused on the Study Segment
+          could better collaborate e.g. formation of “Issue Communities” to
+          avoid duplication of provision/administration (e.g. student
+          on-boarding), merger of initiatives and/or adoption of a ‘baton pass’
+          measures between adjoining age group focussed schemes;
+        </li>
+        <li>
+          Create a summary of gaps in provision e.g. issues in the SM Chronology
+          of Issues not addressed or lack of activity in specific geographic
+          areas;
+        </li>
+        <li>
+          Highlight best in class providers measured on reach and collaboration;
+          and
+        </li>
+        <li>Highlight additional areas flagged for innovation focus.</li>
+      </ul>
+      <p>
+        The Study Segment teams will then present their findings to the ESMA
+        panel in September 2021
+      </p>
+    </main>
+    <aside>
+      <ReliefImage />
+    </aside>
+  </div>
 );
 
 export default Main;
--- src/components/operational-review/relief-image.tsx
+++ src/components/operational-review/relief-image.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import Img from "gatsby-image/withIEPolyfill";
+import { css } from "@emotion/core";
+import { BreakPoint, mq } from "../../util/mq";
 
 export const PureReliefImage = ({ data }) => {
   return (
     <Img
-      fluid={{
-        ...data.placeholderImage.childImageSharp.fluid,
-        aspectRatio: 7 / 5,
-      }}
+      fluid={[
+        data.placeholderImage.default.fluid,
+        {
+          ...data.placeholderImage.tablet.fluid,
+          media: mq(BreakPoint.md),
+        },
+        {
+          ...data.placeholderImage.desktop.fluid,
+          media: mq(BreakPoint.lg),
+        },
+      ]}
       className="hero-image-wrapper"
+      /* This is here so our custom styling applies.
+        otherwise, gatsby-image unhelpfully sets a default
+        of 50% 50% on the element itself.
+      */
+      objectPosition=""
     />
   );
 };
@@ -20,8 +34,18 @@ const ReliefImage = () => {
       placeholderImage: file(
         relativePath: { eq: "operational-review-relief.jpg" }
       ) {
-        childImageSharp {
-          fluid(maxWidth: 800, maxHeight: 800, cropFocus: CENTER) {
+        default: childImageSharp {
+          fluid(cropFocus: CENTER) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+        tablet: childImageSharp {
+          fluid(cropFocus: NORTH) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+        desktop: childImageSharp {
+          fluid(cropFocus: ATTENTION) {
             ...GatsbyImageSharpFluid
           }
         }
