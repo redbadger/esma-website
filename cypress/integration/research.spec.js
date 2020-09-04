8c1aa11346b6d5f452247de16fbc3cf17730bb3c
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-04 15:54:10 +0100

    being precise about which h2 matters in cypress, plus a first sentence check

--- cypress/integration/research.spec.js
+++ cypress/integration/research.spec.js
@@ -10,30 +10,36 @@ describe("Research page", function () {
   it("has Next Up button which takes the user to the next page", function () {
     cy.server();
     cy.visit("/research/early-years/executive-summary");
-    cy.get("h2").contains("Life stage overview");
+    cy.get("h2:first-of-type").contains("Life stage overview");
     cy.contains("Next Up").click();
-    cy.get("h2").contains("Family environment");
+    cy.contains(
+      "The family environment affects a child’s early social and cognitive development in a number of ways."
+    );
+    cy.get("h2:first-of-type").contains("Family environment");
     cy.contains("Next Up").click();
-    cy.get("h2").contains("Housing and the neighbourhood");
+    cy.get("h2:first-of-type").contains("Housing and the neighbourhood");
   });
 
   it("has Previous button which takes the user to the previous page", function () {
     cy.server();
     cy.visit("/research/early-years/housing-and-neighbourhood");
-    cy.get("h2").contains("Housing and the neighbourhood");
+    cy.get("h2:first-of-type").contains("Housing and the neighbourhood");
     cy.contains("Previous").click();
-    cy.get("h2").contains("Family environment");
+    cy.contains(
+      "The family environment affects a child’s early social and cognitive development in a number of ways."
+    );
+    cy.get("h2:first-of-type").contains("Family environment");
     cy.contains("Previous").click();
-    cy.get("h2").contains("Life stage overview");
+    cy.get("h2:first-of-type").contains("Life stage overview");
   });
 
   it("has pills which takes the user to those pages", function () {
     cy.server();
     cy.visit("/research/early-years/executive-summary");
-    cy.get("h2").contains("Life stage overview");
+    cy.get("h2:first-of-type").contains("Life stage overview");
     cy.get("nav").contains("Housing and the neighbourhood").click();
-    cy.get("h2").contains("Housing and the neighbourhood");
+    cy.get("h2:first-of-type").contains("Housing and the neighbourhood");
     cy.get("nav").contains("Impact of Covid-19").click();
-    cy.get("h2").contains("Impact of Covid-19");
+    cy.get("h2:first-of-type").contains("Impact of Covid-19");
   });
 });
