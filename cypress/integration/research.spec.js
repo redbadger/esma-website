27ec1e5c1329443dc9ddf087537d13b4ce1f644e
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-07 17:29:50 +0100

    cypress test covering ResearchNavigation component

--- cypress/integration/research.spec.js
+++ cypress/integration/research.spec.js
@@ -1,4 +1,4 @@
-const thenWeSeeLifeStageOverview = () => {
+const thenWeSeeEarlyYearsLifeStageOverview = () => {
   cy.contains(
     "The time between birth and primary school is a critical time for a child’s social, emotional, cognitive and communication development."
   );
@@ -19,6 +19,20 @@ const thenWeSeeHousing = () => {
   cy.get("h2:first-of-type").contains("Housing and the neighbourhood");
 };
 
+const thenWeSeeSchoolYearsLifeStageOverview = () => {
+  cy.contains(
+    "The school system has been seen as a vital tool to support social mobility"
+  );
+  cy.get("h2:first-of-type").contains("Life Stage Overview");
+};
+
+const thenWeSeeSchoolYearsEarlyYearsFoundationStage = () => {
+  cy.contains(
+    "The Early Years Foundation Stage sets the standards for the learning, development and care of children from birth to five years of age."
+  );
+  cy.get("h2:first-of-type").contains("Early Years Foundation Stage");
+};
+
 const whenWeClickNextUp = () => {
   cy.contains("Next Up").click();
 };
@@ -27,7 +41,7 @@ const whenWeClickPrevious = () => {
   cy.contains("Previous").click();
 };
 
-describe("Research page", function () {
+describe("Issues chronology research page", function () {
   it("has hidden elements in navigation which can be found by clicking the scroller", function () {
     cy.server();
     cy.visit("/issues-chronology/early-years/overview");
@@ -39,7 +53,7 @@ describe("Research page", function () {
   it("has Next Up button which takes the user to the next page", function () {
     cy.server();
     cy.visit("/issues-chronology/early-years/overview");
-    thenWeSeeLifeStageOverview();
+    thenWeSeeEarlyYearsLifeStageOverview();
     whenWeClickNextUp();
     thenWeSeeFamilyEnvironment();
     whenWeClickNextUp();
@@ -53,16 +67,30 @@ describe("Research page", function () {
     whenWeClickPrevious();
     thenWeSeeFamilyEnvironment();
     whenWeClickPrevious();
-    thenWeSeeLifeStageOverview();
+    thenWeSeeEarlyYearsLifeStageOverview();
   });
 
   it("has pills which takes the user to those pages", function () {
     cy.server();
     cy.visit("/issues-chronology/early-years/overview");
-    thenWeSeeLifeStageOverview();
+    thenWeSeeEarlyYearsLifeStageOverview();
     cy.get("nav").contains("Housing and the neighbourhood").click();
     thenWeSeeHousing();
     cy.get("nav").contains("Impact of Covid-19").click();
     cy.get("h2:first-of-type").contains("Impact of Covid-19");
+  });
+
+  it("can navigate to other chronology sections", function () {
+    cy.server();
+    cy.visit("/issues-chronology/early-years/overview");
+    thenWeSeeEarlyYearsLifeStageOverview();
+    cy.get("nav").contains("Housing and the neighbourhood").click();
+    thenWeSeeHousing();
+    cy.get("nav").contains("School years").click();
+    thenWeSeeSchoolYearsLifeStageOverview();
+    whenWeClickNextUp();
+    thenWeSeeSchoolYearsEarlyYearsFoundationStage();
+    cy.get("nav").contains("Early years").click();
+    thenWeSeeEarlyYearsLifeStageOverview();
   });
 });
