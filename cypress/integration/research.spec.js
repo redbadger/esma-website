79381f404ef78d5335bee9cad7e00ae58f43450e
Tornike Keburia <tornike.keburia@red-badger.com>
2020-09-07 10:08:47 +0100

    fix cypress test

--- cypress/integration/research.spec.js
+++ cypress/integration/research.spec.js
@@ -30,7 +30,7 @@ const whenWeClickPrevious = () => {
 describe("Research page", function () {
   it("has hidden elements in navigation which can be found by clicking the scroller", function () {
     cy.server();
-    cy.visit("/research/early-years/executive-summary");
+    cy.visit("/research/early-years/overview");
     cy.contains("Working life").then(el => Cypress.dom.isHidden(el));
     cy.get(".scroll-help.right").click();
     cy.contains("Working life").then(el => !Cypress.dom.isHidden(el));
@@ -38,7 +38,7 @@ describe("Research page", function () {
 
   it("has Next Up button which takes the user to the next page", function () {
     cy.server();
-    cy.visit("/research/early-years/executive-summary");
+    cy.visit("/research/early-years/overview");
     thenWeSeeLifeStageOverview();
     whenWeClickNextUp();
     thenWeSeeFamilyEnvironment();
@@ -58,7 +58,7 @@ describe("Research page", function () {
 
   it("has pills which takes the user to those pages", function () {
     cy.server();
-    cy.visit("/research/early-years/executive-summary");
+    cy.visit("/research/early-years/overview");
     thenWeSeeLifeStageOverview();
     cy.get("nav").contains("Housing and the neighbourhood").click();
     thenWeSeeHousing();
