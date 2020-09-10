73b1c1977a5a516c3e30125b986a83b825bdfc92
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-09-10 11:32:44 +0100

    using grid for newsletter signup, correcting spacing

--- src/components/newsletter/newsletter-input-field.tsx
+++ src/components/newsletter/newsletter-input-field.tsx
@@ -1,42 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
 
-const newsletterInputFieldStyles = css`
-  label {
-    display: block;
-    font-weight: 600;
-    color: var(--midnight);
-  }
-
-  .input-error {
-    color: var(--error);
-    line-height: 1.625rem;
-    margin-top: 0.75rem;
-    display: none;
-  }
-
-  input.error + .input-error {
-    display: block;
-  }
-
-  input {
-    border: 1px solid var(--midnight);
-    border-radius: 3px;
-    width: 100%;
-    height: 3rem;
-    padding: 7px 18px 7px 10px;
-
-    &:focus {
-      outline: none;
-      border-color: var(--cobalt);
-    }
-  }
-
-  input.error {
-    border-color: var(--error);
-  }
-`;
-
 type NewsletterInputFieldProps = {
   label: string;
   type: string;
@@ -57,20 +21,22 @@ const NewsletterInputField = ({
   valueSetter,
 }: NewsletterInputFieldProps) => {
   return (
-    <section css={newsletterInputFieldStyles}>
-      <label htmlFor={`${name}Input`}>{label}</label>
-      <input
-        type={type}
-        name={name}
-        id={`${name}Input`}
-        className={error ? "error" : ""}
-        onChange={e => {
-          errorSetter(false);
-          valueSetter(e.target.value);
-        }}
-      />
-      <p className="input-error">{errorMessage}</p>
-    </section>
+    <>
+      <div>
+        <label htmlFor={`${name}Input`}>{label}</label>
+        <input
+          type={type}
+          name={name}
+          id={`${name}Input`}
+          className={error ? "error" : ""}
+          onChange={e => {
+            errorSetter(false);
+            valueSetter(e.target.value);
+          }}
+        />
+      </div>
+      <p className={`input-error ${error ? "error" : ""}`}>{errorMessage}</p>
+    </>
   );
 };
 
