20251b012923cfdf9b94096d0794084959d5ff61
Tornike Keburia <tornike.keburia@red-badger.com>
2020-09-11 18:37:02 +0100

    graph fixes

--- src/components/research/label-graph-pair.tsx
+++ src/components/research/label-graph-pair.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/core";
 
 const containerStyles = css`
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 2fr 3fr;
   column-gap: 1.75rem;
 
   svg {
@@ -14,7 +14,7 @@ const containerStyles = css`
 
   div.label-wrapper {
     border-bottom: 1px solid var(--separator);
-    min-height: 10rem;
+    height: 10rem;
     display: flex;
     align-items: center;
   }
