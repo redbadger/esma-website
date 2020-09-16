8088acef50127919e68c3682676a28bb90668065
Tornike Keburia <tornike.keburia@red-badger.com>
2020-09-16 17:43:51 +0100

    add commitments section

--- src/components/homepage/commitments-entry.tsx
+++ src/components/homepage/commitments-entry.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { css } from "@emotion/core";
+import { mq, BreakPoint, screenSizes } from "../../util/mq";
+import useWindowSize from "../../util/useWindowSize";
 
 const commitmentsEntryStyles = css`
+  margin-top: 5.25rem;
   font-size: 1.125rem;
   line-height: 1.875rem;
   text-align: left;
 
   > div {
-    column-gap: 11.75rem;
+    column-gap: 1rem;
     display: grid;
   }
 
   .right-shift {
     grid-template-columns: 6fr 5fr;
+
+    .homepage-image-wrapper,
+    svg {
+      justify-self: end;
+    }
   }
 
   .left-shift {
     grid-template-columns: 5fr 6fr;
+
+    .homepage-image-wrapper,
+    svg {
+      justify-self: start;
+    }
   }
 
   h3 {
@@ -24,7 +37,52 @@ const commitmentsEntryStyles = css`
     line-height: 1.625rem;
     font-weight: 600;
     margin-bottom: 0.75rem;
-    margin-top: 1.25rem;
+  }
+
+  .homepage-image-wrapper {
+    width: 100%;
+    max-width: 19rem;
+    max-height: 17.75rem;
+    ${mq(BreakPoint.lg)} {
+      width: 100%;
+      max-width: 25rem;
+      max-height: 23.75rem;
+    }
+  }
+
+  svg {
+    max-height: 19.5rem;
+    max-width: 19.5rem;
+  }
+`;
+
+const commonStyles = css`
+  .coming-soon {
+    font-weight: 600;
+    margin-top: -1.5rem;
+    display: block;
+    margin-bottom: 2.25rem;
+  }
+
+  p {
+    margin-bottom: 2.25rem;
+  }
+
+  h3 {
+    font-weight: 600;
+    margin-bottom: 0.75rem;
+  }
+`;
+
+const mobileStyles = css`
+  margin-bottom: 3.875rem;
+  h3 {
+    font-size: 1.125rem;
+    line-height: 2.125rem;
+  }
+
+  svg {
+    max-width: 100%;
   }
 `;
 
@@ -39,8 +97,19 @@ const CommitmentsEntry = ({
   content,
   shiftRight,
 }: CommitmentsEntryProps) => {
+  const { width } = useWindowSize();
+
+  if (width < screenSizes[BreakPoint.md]) {
+    return (
+      <div css={[commonStyles, mobileStyles]}>
+        {content}
+        {image}
+      </div>
+    );
+  }
+
   return (
-    <div css={commitmentsEntryStyles}>
+    <div css={[commonStyles, commitmentsEntryStyles]}>
       <div className={shiftRight ? "right-shift" : "left-shift"}>
         {!shiftRight && (
           <>
--- src/components/homepage/commitments.tsx
+++ src/components/homepage/commitments.tsx
@@ -3,32 +3,48 @@ import CommitmentsEntry from "./commitments-entry";
 import HomepageImage from "./homepage-image";
 import { css } from "@emotion/core";
 import { Link } from "gatsby";
+import ActivityMapSVG from "../../svg/activity-map.svg";
+import { mq, BreakPoint } from "../../util/mq";
 
 const commitmentsStyles = css`
   color: var(--midnight);
   background-color: var(--taupe);
-  padding-left: 10.3125rem;
-  padding-right: 10.3125rem;
   text-align: center;
-  padding-top: 4.5rem;
+  padding: 2.25rem 0.75rem;
 
-  h2 {
-    font-size: 2rem;
-    line-height: 2.75rem;
-    font-weight: 600;
-    margin-bottom: 4.5rem;
+  ${mq(BreakPoint.md)} {
+    padding: 4.5rem 2.4375rem;
   }
 
-  p {
-    margin-bottom: 36px;
+  ${mq(BreakPoint.lg)} {
+    padding: 4.5rem 10.3125rem;
+  }
+
+  h2 {
+    ${mq(BreakPoint.md)} {
+      font-size: 2rem;
+      margin-bottom: 4.5rem;
+    }
+    font-size: 1.25rem;
+    line-height: 2.75rem;
+    font-weight: 600;
+    margin-bottom: 2.25rem;
+
   }
 
   a {
+    display: block;
+
+    ${mq(BreakPoint.md)} {
+      display: inline;
+    }
+
     border: 1px solid var(--midnight);
     padding: 12px 108px;
     line-height: 1.875rem;
     font-weight: 700;
     white-space: nowrap;
+    margin-bottom: 2.25rem;
   }
 `;
 
@@ -52,17 +68,57 @@ const commitmentsData = [
   {
     content: (
       <div>
-        <h3>Life stages research</h3>
+        <h3>Incubator Fund</h3>
         <p>
-          Create a consolidated appraisal of the existing academic research
-          relating to the issues affecting an individual’s employability (from
-          cradle to retirement) and host it on an open source digital chronology
-          to enable a quick easy to navigate understanding of the issues.
+          Subject to funding, create a Social Mobility Incubator Fund 100% of
+          which will be used to licence (on a commercial basis) best practice
+          materials from organisations identified by the SM Operational
+          Appraisal.
         </p>
-        <Link to="/issues-chronology">Read research</Link>
+        <p>
+          These materials will be provided to other Social Mobility focussed
+          organisations on a Creative Commons basis, in turn, cross fertilising
+          best practice across the UK and enabling exemplar
+          initiatives/organisations to grow and increase impact.
+        </p>
+        <span className="coming-soon">Coming soon</span>
       </div>
     ),
-    image: <HomepageImage imageName="life-stage-research.png" />,
+    image: <HomepageImage imageName="incubator-fund.jpg" />,
+    shiftRight: true,
+  },
+  {
+    content: (
+      <div>
+        <h3>Activity Heat Map</h3>
+        <p>
+          Conduct a review of Business, State and Third Sector efforts across
+          the UK to combat the issues identified in SM Issues Chronology and
+          produce an open source digital heat map to enable users of the ESMA
+          site to identify how they can collaborate with existing provision to
+          maximise impact.
+        </p>
+        <span className="coming-soon">Coming September 2021</span>
+        <Link to="/activity-map">Find out more</Link>
+      </div>
+    ),
+    image: <ActivityMapSVG />,
+    shiftRight: false,
+  },
+  {
+    content: (
+      <div>
+        <h3>Best Practice</h3>
+        <p>
+          Conduct an operational appraisal of the organisations and initiatives
+          mapped in the SM Activity Heat Map and suggest how they could increase
+          operational efficiency through collaboration locally and nationally
+        </p>
+        <span className="coming-soon">Coming September 2021</span>
+        <Link to="/operational-review">Find out more</Link>
+      </div>
+    ),
+    image: <HomepageImage imageName="best-practice.jpg" />,
     shiftRight: true,
   },
 ];
