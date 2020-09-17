5fa795fcbf3ce7ecdf94b546f9a5c28c65322bdd
Tornike Keburia <tornike.keburia@red-badger.com>
2020-09-17 16:15:16 +0100

    update best practice intro copy

--- src/pages/best-practice.tsx
+++ src/pages/best-practice.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import Layout from "../components/common/layout";
 import SEO from "../components/common/seo";
 
-import OperationalReviewHero from "../components/best-practice/hero";
-import OperationalReviewMain from "../components/best-practice/main";
+import BestPracticeHero from "../components/best-practice/hero";
+import BestPracticeMain from "../components/best-practice/main";
 import NewsletterSignUp from "../components/newsletter/newsletter-sign-up";
 import EsmaPanel from "../components/best-practice/esma-panel";
 
 import panel from "../components/best-practice/panel";
 
-const OperationalReview = () => {
+const BestPractice = () => {
   return (
     <Layout>
       <SEO title="Best Practice" />
-      <OperationalReviewHero />
-      <OperationalReviewMain />
+      <BestPracticeHero />
+      <BestPracticeMain />
       <EsmaPanel panel={panel} />
       <NewsletterSignUp />
     </Layout>
   );
 };
 
-export default OperationalReview;
+export default BestPractice;
