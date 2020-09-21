aaebaabd30ff73a518a4b75983b14da195265f3b
Tornike Keburia <tornike.keburia@red-badger.com>
2020-09-21 17:46:33 +0100

    terms of use page

---
+++ src/pages/terms-of-use.tsx
@@ -0,0 +1,491 @@
+import React from "react";
+import Layout from "../components/common/layout";
+import SEO from "../components/common/seo";
+import { css } from "@emotion/core";
+
+const termsOfUseStyles = css`
+  text-align: left;
+  color: var(--midnight);
+  padding: 4.5rem 4.5rem;
+  font-size: 1.125rem;
+
+  a {
+    text-decoration: underline;
+    font-weight: 600;
+  }
+
+  h3 {
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  p {
+    margin-top: 1rem;
+    font-size: 1.125rem;
+  }
+
+  ul {
+    margin-top: 1rem;
+    margin-left: 2rem;
+    list-style-type: disc;
+  }
+
+  ol {
+    list-style-position: inside;
+    list-style-type: decimal;
+    text-align: left;
+    margin-top: 2rem;
+
+    li {
+      font-weight: 600;
+    }
+  }
+`;
+
+const TermsOfUse = () => {
+  return (
+    <Layout>
+      <SEO title="Privacy Policy" />
+      <div css={termsOfUseStyles}>
+        <h3>ESMA Platform Terms of Use</h3>
+        <ol start={1}>
+          <li>Introduction</li>
+        </ol>
+        <p>
+          This Platform (defined below) is operated on behalf of the employers
+          social mobility alliance by{" "}
+          <a href="https://red-badger.com">Red Badger Consulting Ltd</a>. The
+          employers social mobility alliance (“ESMA”) is an unincorporated
+          association which is dedicated to promoting a coordinated and
+          collaborative response across State, Business and Third Sector to
+          address the issues faced by individuals from low income backgrounds
+          when pursuing their career goals.
+        </p>
+        <ol start={2}>
+          <li>What’s in these terms</li>
+        </ol>
+        <p>
+          These terms tell you the rules you accept to comply with when using
+          services on our platform{" "}
+          <a href="https://esma.community">esma.community</a> (the “Platform”).
+          Please read these terms of use carefully before using this Platform.
+        </p>
+
+        <ol start={3}>
+          <li>By using this Platform you accept these terms</li>
+        </ol>
+        <p>
+          By using our Platform, you confirm that you accept these terms of use
+          and that you agree to comply with them. If you do not agree to these
+          terms, you must not use our Platform. We recommend that you print a
+          copy of these terms for future reference.
+        </p>
+
+        <ol start={4}>
+          <li>
+            These terms of use incorporate the ESMA Privacy and Cookie Policy
+          </li>
+        </ol>
+        <p>
+          <a href="https://esma.community/privacy-policy">
+            Our Privacy and Cookie Policy
+          </a>
+          , sets out the terms on which we process any personal data we collect
+          from you, or that you provide to us. By using our Platform, you
+          consent to such processing and you warrant that all data provided by
+          you is accurate.
+        </p>
+
+        <ol start={5}>
+          <li>We may make changes to these terms</li>
+        </ol>
+
+        <p>
+          We may update these terms of use from time to time. It is your
+          responsibility to ensure you review these terms regularly to
+          familiarise yourself with any changes. Your use of our Platform
+          following any such changes will constitute your acceptance of the
+          revised terms. These terms were most recently updated on 23 September
+          2020.
+        </p>
+
+        <ol start={6}>
+          <li>We may suspend or withdraw your use of this Platform</li>
+        </ol>
+        <p>
+          Our Platform is made available free of charge. We do not guarantee
+          that our Platform, or any content on it, will always be available or
+          be uninterrupted. We may suspend or withdraw or restrict the
+          availability of all or any part of our Platform at our sole and
+          absolute discretion. We may do this without notice to you.
+        </p>
+
+        <p>
+          We reserve the right to suspend, restrict or terminate your access to
+          all or any part of our Platform at any time at our sole and absolute
+          discretion. We may also permanently ban or temporarily suspend you
+          from using our Platform if you do not comply with these terms of use,
+          at our sole and absolute discretion.
+        </p>
+        <p>
+          You are also responsible for ensuring that all persons who access our
+          Platform through your internet connection are aware of these terms of
+          use and other applicable terms and conditions, and that they comply
+          with them.
+        </p>
+
+        <ol start={7}>
+          <li>How you may use material on our Platform</li>
+        </ol>
+        <p>
+          ESMA is the owner or the licensee of all intellectual property rights
+          in our Platform, and (unless otherwise stated) in the material
+          published on it. Those works are protected by copyright laws and
+          treaties around the world. All such rights are reserved.
+        </p>
+        <p>
+          You may print off one copy, and may download extracts, of any page(s)
+          from our Platform for your personal use and you may draw the attention
+          of others within your organization or learning institution to content
+          posted on our Platform.
+        </p>
+        <p>
+          You must not modify the paper or digital copies of any materials you
+          have printed off or downloaded in any way, and you must not use any
+          illustrations, photographs, video or audio sequences or any graphics
+          separately from any accompanying text.
+        </p>
+        <p>
+          The status of ESMA (and that of any identified contributors) as the
+          authors of content on our Platform must always be acknowledged when
+          use is permitted in accordance with this paragraph 7 of these terms of
+          use.
+        </p>
+        <p>
+          You must not use any part of the content on our Platform for
+          commercial purposes.
+        </p>
+        <p>
+          If you print off, copy or download any part of our Platform in breach
+          of these terms of use, your right to use our Platform will cease
+          immediately and you must, at our option, return or destroy any copies
+          of the materials you have made.
+        </p>
+
+        <p>
+          You may not assign, sub-license or otherwise transfer any or your
+          rights or obligations set out in these terms of use to any other
+          person.
+        </p>
+
+        <ol start={8}>
+          <li>Do not rely on information on this Platform</li>
+        </ol>
+        <p>
+          The content on our Platform is provided for general information only.
+          It is not intended to amount to advice on which you should rely.
+          Consequently, we do not accept any responsibility or liability for any
+          actions or omissions that you may take in reliance on such content.
+          You must obtain professional or specialist advice before taking, or
+          refraining from, any action on the basis of the content on our
+          Platform.
+        </p>
+
+        <p>
+          Although we make reasonable efforts to update the information on our
+          Platform, we make no representations, warranties or guarantees,
+          whether express or implied, that the content on our Platform is
+          accurate, complete or up to date.
+        </p>
+
+        <ol start={9}>
+          <li>
+            We are not responsible for the websites or third party platforms we
+            link to
+          </li>
+        </ol>
+        <p>
+          Where our Platform contains links to other websites or platforms and
+          resources provided by third parties, these links are provided for your
+          information only. Such links should not be interpreted as approval by
+          us of those linked websites, platforms, resources or information you
+          may obtain from them. We have no control over the contents of those
+          websites, platforms or resources.
+        </p>
+
+        <ol start={10}>
+          <li>Our responsibility for loss or damage suffered by you</li>
+        </ol>
+        <p>
+          <strong>Whether you are a consumer or a business user:</strong>
+        </p>
+        <p>
+          We do not exclude or limit in any way our liability to you where it
+          would be unlawful to do so. This includes liability for death or
+          personal injury caused by our negligence or the negligence of our
+          employees, agents or subcontractors and for fraud or fraudulent
+          misrepresentation.
+        </p>
+        <p>
+          <strong>If you are a business user:</strong>
+        </p>
+        <p>
+          We exclude all implied conditions, warranties, representations or
+          other terms that may apply to our Platform or any content on it
+        </p>
+        <p>
+          We will not be liable to you for any loss or damage, whether in
+          contract, tort (including negligence), breach of statutory duty, or
+          otherwise, even if foreseeable, arising under or in connection with:
+        </p>
+
+        <ul>
+          <li>use of, or inability to use, our Platform; or</li>
+          <li>use of or reliance on any content displayed on our Platform.</li>
+        </ul>
+        <p>In particular and without limitation, we will not be liable for:</p>
+        <ul>
+          <li>loss of revenue, profits, sales or business;</li>
+          <li>business interruption;</li>
+          <li>loss of anticipated savings;</li>
+          <li>loss of business opportunity, goodwill or reputation; or</li>
+          <li>any direct, indirect or consequential loss or damage.</li>
+        </ul>
+        <p>
+          <strong>If you are a consumer user:</strong>
+        </p>
+        <p>
+          Nothing in these terms will restrict your statutory rights as a
+          consumer (including your rights to receive a reasonable standard of
+          service, more details about which can be obtained from your local
+          Trading Standards Office or Citizen’s Advice Bureau).
+        </p>
+        <p>
+          Please note that we only provide our Platform for domestic and private
+          use. You agree not to use our Platform for any commercial or business
+          purposes, and we have no liability to you for any loss of revenue,
+          profit, loss of business, business interruption, or loss of business
+          opportunity.
+        </p>
+        <p>
+          <strong>All users:</strong>
+        </p>
+        <p>
+          <strong>
+            We are not responsible for viruses and you must not introduce them
+          </strong>
+        </p>
+        <p>
+          We do not guarantee that our Platform will be secure or free from bugs
+          or viruses.
+        </p>
+        <p>
+          You are responsible for configuring your information technology,
+          computer programmes and platform to access our Platform. You should
+          use your own virus protection software.
+        </p>
+        <p>
+          You must not misuse our Platform by knowingly introducing viruses,
+          trojans, worms, logic bombs or other material that is malicious or
+          technologically harmful. You must not attempt to gain unauthorised
+          access to our Platform, the server on which our Platform is stored or
+          any server, computer or database connected to our Platform. You must
+          not attack our Platform via a denial-of-service attack or a
+          distributed denial-of service attack. By breaching this provision, you
+          would commit a criminal offence under the Computer Misuse Act 1990. We
+          will report any such breach to the relevant law enforcement
+          authorities and we will co-operate with those authorities by
+          disclosing your identity to them. In the event of such a breach, your
+          right to use our Platform will cease immediately.
+        </p>
+
+        <ol start={11}>
+          <li>Rules about linking to our Platform</li>
+        </ol>
+        <p>
+          You may link to our home page, provided you do so in a way that is
+          fair and legal and does not damage our reputation or take advantage of
+          it.
+        </p>
+        <p>
+          You must not establish a link in such a way as to suggest any form of
+          association, approval or endorsement on our part where none exists.
+        </p>
+        <p>
+          You must not establish a link to our Platform in any website or
+          platform that is not owned by you. Our Platform must not be framed on
+          any other website or platform, nor may you create a link to any part
+          of our Platform other than the home page.
+        </p>
+        <p>
+          We reserve the right to withdraw linking permission without notice.
+        </p>
+        <p>
+          If you wish to link to or make any use of content on our Platform
+          other than that set out above, please email us at{" "}
+          <a href="mailto:barrymurphy@esma.community">
+            barrymurphy@esma.community
+          </a>
+        </p>
+        <ol start={12}>
+          <li>Prohibited uses</li>
+        </ol>
+        <p>
+          You may use our Platform only for lawful purposes. You may not use our
+          Platform:
+        </p>
+        <ul>
+          <li>
+            in any way that breaches any applicable local, national or
+            international law or regulation;
+          </li>
+          <li>
+            in any way that is unlawful or fraudulent, or has any unlawful or
+            fraudulent purpose or effect;
+          </li>
+          <li>
+            for the purpose of harming or attempting to harm minors in any way;
+          </li>
+          <li>
+            to send, knowingly receive, upload, download, use or re-use any
+            material which does not comply with these terms of use;
+          </li>
+          <li>
+            to transmit, or procure the sending of, any unsolicited or
+            unauthorised advertising or promotional material or any other form
+            of similar solicitation (spam); or
+          </li>
+          <li>
+            to knowingly transmit any data, send or upload any material that
+            contains viruses, Trojan horses, worms, time-bombs, keystroke
+            loggers, spyware, adware or any other harmful programs or similar
+            computer code designed to adversely affect the operation of any
+            computer software or hardware.
+          </li>
+        </ul>
+        <p>You also agree:</p>
+        <ul>
+          <li>
+            not to access without authority, interfere with, damage or disrupt:
+          </li>
+          <ul>
+            <li>any part of our Platform;</li>
+            <li>any equipment or network on which our Platform is stored;</li>
+            <li>any software used in the provision of our Platform; or</li>
+            <li>
+              any equipment or network or software owned or used by any third
+              party.
+            </li>
+          </ul>
+        </ul>
+
+        <ol start={13}>
+          <li>Contribution standards</li>
+        </ol>
+        <p>
+          The contribution standards in this paragraph 13 apply to any and all
+          material which you contribute to our Platform. You must comply with
+          the spirit and the letter of the following standards. The standards
+          apply to each part of any contribution as well as to its whole.
+        </p>
+        <p>Contributions must:</p>
+        <ul>
+          <li>be accurate (where they state facts);</li>
+          <li>be genuinely held (where they state opinions); and</li>
+          <li>
+            comply with applicable law in the UK and in any country from which
+            they are posted.
+          </li>
+        </ul>
+        <p>Contributions must not:</p>
+        <ul>
+          <li>contain any material which is defamatory of any person;</li>
+          <li>
+            contain any material which is obscene, offensive, hateful or
+            inflammatory;
+          </li>
+          <li>promote sexually explicit material;</li>
+          <li>promote violence;</li>
+          <li>
+            promote discrimination based on race, sex, religion, nationality,
+            disability, sexual orientation or age;
+          </li>
+          <li>
+            infringe any copyright, database right or trademark of any other
+            person;
+          </li>
+          <li>be likely to deceive any person;</li>
+          <li>
+            be made in breach of any legal duty owed to a third party, such as a
+            contractual duty or a duty of confidence;
+          </li>
+          <li>promote any illegal activity;</li>
+          <li>
+            be threatening, abuse or invade another’s privacy, or cause
+            annoyance, inconvenience or needless anxiety;
+          </li>
+          <li>
+            be likely to harass, upset, embarrass, alarm or annoy any other
+            person;
+          </li>
+          <li>
+            be used to impersonate any person, or to misrepresent your identity
+            or affiliation with any person;
+          </li>
+          <li>
+            give the impression that they emanate from us, if this is not the
+            case; or
+          </li>
+          <li>
+            advocate, promote or assist any unlawful act such as (by way of
+            example only) copyright infringement or computer misuse.
+          </li>
+        </ul>
+
+        <ol start={14}>
+          <li>
+            What happens if we do not enforce a right under these terms of use
+            against you?
+          </li>
+        </ol>
+        <p>
+          If we decide not to exercise or enforce a right that we have against
+          you (e.g. as a result of you breaching these terms), this does not
+          prevent us from doing so at a later date.
+        </p>
+        <ol start={15}>
+          <li>What happens if part of these terms is not enforceable?</li>
+        </ol>
+        <p>
+          If any provision of these terms is found by any court or
+          administrative body of competent jurisdiction to be invalid or
+          unenforceable then that invalid or unenforceable part will be deemed
+          deleted from these terms, such invalidity or unenforceability will not
+          affect the other provisions of these terms and they will remain in
+          full force and effect.
+        </p>
+
+        <ol start={16}>
+          <li>Rules about linking to our Platform</li>
+        </ol>
+        <p>
+          These terms of use do not create any rights that are enforceable by
+          any person who is not a party to these terms.
+        </p>
+
+        <ol start={17}>
+          <li>Which country’s laws apply to any disputes?</li>
+        </ol>
+        <p>
+          Please note that these terms of use, their subject matter and their
+          formation, are governed by English and Welsh law. You and we both
+          agree that the courts of England and Wales will have exclusive
+          jurisdiction.
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default TermsOfUse;
