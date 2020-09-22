6022a7c14fc2e31744c9d1de3c766189d5aefe69
Sam Taylor <sjltaylor@gmail.com>
2020-09-22 15:20:01 +0100

    remove the newsletter until we're worked through privacy policy

--- src/components/newsletter/newsletter-input-field.tsx
+++ src/components/newsletter/newsletter-input-field.tsx
@@ -6,6 +6,7 @@ type NewsletterInputFieldProps = {
   type: string;
   name: string;
   error: boolean;
+  disabled: boolean;
   errorMessage: string;
   errorSetter: (isError: boolean) => void;
   valueSetter: (value: string) => void;
@@ -16,6 +17,7 @@ const NewsletterInputField = ({
   label,
   type,
   name,
+  disabled,
   error,
   errorMessage,
   errorSetter,
@@ -30,6 +32,7 @@ const NewsletterInputField = ({
           type={type}
           name={name}
           id={`${name}Input`}
+          disabled={disabled}
           className={error ? "error" : ""}
           onChange={e => {
             errorSetter(false);
